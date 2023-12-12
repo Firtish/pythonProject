8a8d8c6ebbb8f9c8a614b29a6b1df9d5fe7b4db2
abojaczuk <abojaczuk@fmlogistic.com>
2023-12-12 12:19:13 +0100

    bazy

--- bazy5.py
+++ bazy5.py
@@ -12,4 +12,57 @@ class Person(Base):
     name = Column(String)
     age = Column(String)
     addresses = relationship('Address',
-                              back_populates='person')
+                             back_populates='person',
+                             order_by='Address.email',
+                             cascade='all, delete-orphan')
+
+    def __repr__(self):
+        return f"{self.name} (id={self.id})"
+
+
+class Address(Base):
+    __tablename__ = 'address'
+    id = Column(Integer, primary_key=True)
+    email = Column(String)
+    person_id = Column(ForeignKey('person.id'))
+    person = relationship("Person", back_populates='addresses')
+
+    def __repr__(self):
+        return self.email
+
+
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+
+session = Session()
+
+anakin = Person(name='Anakin', age=38)
+obi = Person(name="Obi Wan Kenobi", age=45)
+
+obi.addresses = [
+    Address(email='obi@example.com'),
+    Address(email='waka@wp.pl')
+
+]
+obi2 = Person(name="obi", age=45)
+session.add(anakin)
+session.add(obi)
+session.add(obi2)
+
+session.commit()
+
+all_ = session.query(Person).all()
+print(all_)
+
+an1 = session.query(Person).first()
+print(an1)
+print(an1.id, an1.name, an1.addresses)  # 1 Anakin []
+
+obi_list = session.query(Person).filter(
+    Person.name.like('Obi%')
+).all()
+print(obi_list)
+for o in obi_list:
+    print("id=", o.name, "age:", o.age, "email:", o.addresses)
+# id= Obi Wan Kenobi age: 45 email: [obi@example.com, waka@wp.pl]
+# id= obi age: 45 email: []
